9792809810ebecc76b62514d3ced198e3a7ea2f0
hugomatheus <hg.targino@gmail.com>
2020-08-19 09:22:20 -0300

    Realizando o desafio 03 - reactjs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from "react";
 
 import "./styles.css";
+import { useEffect } from "react";
+import api from "./services/api";
+import { useState } from "react";
 
 function App() {
   async function handleAddRepository() {
-    // TODO
+    try {
+      const response = await api.post("/repositories", {
+        title: "My repository",
+        url: "https://github.com/hugomatheus",
+        techs: ["PHP", "Node.js", "ReactJS", "React Native"],
+      });
+      setRepositories([...repositories, response.data]);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`/repositories/${id}`);
+      const repositoryIndex = repositories.findIndex(
+        (repository) => repository.id === id
+      );
+      repositories.splice(repositoryIndex, 1);
+      setRepositories([...repositories]);
+      // other way 
+      // const newRepositories = repositories.filter(repository => repository.id !== id);
+      // setRepositories(newRepositories);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
+  useEffect(() => {
+    api.get("/repositories").then((response) => {
+      setRepositories([...response.data]);
+    });
+  }, []);
+  const [repositories, setRepositories] = useState([]);
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
